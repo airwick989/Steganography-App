09e48f1d475378a9d22b8b7de7f9b9c31bc6a796
airwick989 <73313597+airwick989@users.noreply.github.com>
2024-02-25 15:40:14 -0500

    Implemented stegonagraphic encoding and decoding

--- App/AES_Steganography.py
+++ App/AES_Steganography.py
@@ -7,7 +7,8 @@ class AES_Steg:
     def __init__(self, secrets_path):
         self.aes_key = None
         self.aes_iv = None
-        self.delimiter = None
+        self.start_delimiter = None
+        self.end_delimiter = None
         self.set_secrets(secrets_path)
         aes_secrets = {
             "key": self.aes_key,
@@ -22,7 +23,8 @@ class AES_Steg:
             secrets = {
                 "aes_key": ast.literal_eval(contents[0].strip()),
                 "aes_iv": ast.literal_eval(contents[1].strip()),
-                "secret_delimiter": contents[2].strip()
+                "start_delimiter": contents[2].strip(),
+                "end_delimiter": contents[3].strip()
             }
     
         return secrets
@@ -32,7 +34,8 @@ class AES_Steg:
         secrets = self.get_secrets(secrets_path)
         self.aes_key = secrets['aes_key'].ljust(32, b'\0')  # Pad the key with zeros to make it 32 bytes
         self.aes_iv = secrets['aes_iv']
-        self.delimiter = secrets['secret_delimiter']
+        self.start_delimiter = secrets['start_delimiter']
+        self.end_delimiter = secrets['end_delimiter']
 
 
     def encrypt(self, plaintext):
@@ -41,20 +44,69 @@ class AES_Steg:
     
 
     def decrypt(self, cipher):
-        plaintext = self.aes.encrypt(cipher)
+        plaintext = self.aes.decrypt(cipher)
         return plaintext
+    
+
+    def to_bin(self, message):
+        binary = ''.join(format(ord(char), '08b') for char in message)
+        return binary
+
+    def to_txt(self, binary):
+        txt = ''.join(chr(int(binary[i:i+8], 2)) for i in range(0, len(binary), 8))
+        return txt
 
 
-    pass
+    def embed(self, image_path, plaintext, output_path):
+        msg = self.encrypt(plaintext)
+
+        img = Image.open(image_path)
+        pixels = list(img.getdata())
+        bin_msg = self.to_bin(self.start_delimiter) + self.to_bin(msg) + self.to_bin(self.end_delimiter)
+
+        binary_message_index = 0
+
+        for i in range(len(pixels)):
+            pixel = list(pixels[i])
+            for j in range(3):  # For RGB channels
+                if binary_message_index < len(bin_msg):
+                    pixel[j] = pixel[j] & ~1 | int(bin_msg[binary_message_index])
+                    binary_message_index += 1
+
+            pixels[i] = tuple(pixel)
+
+        img.putdata(pixels)
+        img.save(output_path)
+
+
+    def extract(self, image_path):
+        img = Image.open(image_path)
+        pixels = list(img.getdata())
+
+        binary_message = ''
+
+        for i in range(len(pixels)):
+            pixel = list(pixels[i])
+            for j in range(3):  # For RGB channels
+                binary_message += str(pixel[j] & 1)
+
+        start_delimiter = self.to_bin(self.start_delimiter)
+        end_delimiter = self.to_bin(self.end_delimiter)
+
+        start_index = binary_message.find(start_delimiter) + len(start_delimiter)
+        end_index = binary_message.find(end_delimiter)
+
+        extracted_message = self.to_txt(binary_message[start_index:end_index])
+        return extracted_message
 
 
 
 
-# plaintext = "Hello there, don't mind if I do! I will certainly eat your croissant :)"
+plaintext = "Hello there, don't mind if I do! I will certainly eat your croissant :)"
 steg = AES_Steg("secrets.txt")
-# print(steg.encrypt(plaintext))
-# steg.embed("image.png", plaintext, "encoded_image.png")
-steg.extract("encoded_image.png")
+#steg.embed("image.png", plaintext, "encoded_image.png")
+extracted = steg.extract('encoded_image.png')
+print(f"EXTRACTED: {steg.decrypt(extracted)}")
 
 
 # encrypted_text = steg.aes.encrypt(plaintext)
