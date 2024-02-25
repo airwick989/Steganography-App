108c41e89cab1cbba0f46d844d956c7ded2d0962
airwick989 <73313597+airwick989@users.noreply.github.com>
2024-02-25 12:44:43 -0500

    Update generate_secrets.py

--- App/AES/generate_secrets.py
+++ App/AES/generate_secrets.py
@@ -7,11 +7,11 @@ def generate_aes_key():
 def generate_random_iv():
     return secrets.token_bytes(16)  #generate a random initialization vector (16 bytes for AES)
 
+
 if len(sys.argv) == 1:
     PATH = "secrets.txt"
 else:
     PATH = str(sys.argv[1])
-
 with open(PATH, "w+") as file:
     file.write(f"{str(generate_aes_key())}\n")
     file.write(str(generate_random_iv()))
