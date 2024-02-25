a76cf18af2da947bec296b631925b31c1c44f5e1
airwick989 <73313597+airwick989@users.noreply.github.com>
2024-02-25 13:30:10 -0500

    Modified secret generation

--- App/AES_Steganography.py
+++ App/AES_Steganography.py
@@ -1,5 +1,45 @@
-
+import ast
+from AES import AES
 
 class AES_Steg:
     def __init__(self, secrets_path):
-        pass
+        self.aes_key = None
+        self.aes_iv = None
+        self.delimiter = None
+
+        self.set_secrets(secrets_path)
+
+        aes_secrets = {
+            "key": self.aes_key,
+            "iv": self.aes_iv
+        }
+        self.aes = AES(secrets=aes_secrets)
+
+
+    def get_secrets(self, path):
+        with open(path, 'r') as file:
+            contents = file.readlines()
+            secrets = {
+                "aes_key": ast.literal_eval(contents[0].strip()),
+                "aes_iv": ast.literal_eval(contents[1].strip()),
+                "secret_delimiter": contents[2].strip()
+            }
+    
+        return secrets
+    
+
+    def set_secrets(self, secrets_path):
+        secrets = self.get_secrets(secrets_path)
+        self.aes_key = secrets['aes_key'].ljust(32, b'\0')  # Pad the key with zeros to make it 32 bytes
+        self.aes_iv = secrets['aes_iv']
+        self.delimiter = secrets['secret_delimiter']
+
+
+
+
+steg = AES_Steg("secrets.txt")
+plaintext = "Hello there, don't mind if I do! I will certainly eat your croissant :)"
+encrypted_text = steg.aes.encrypt(plaintext)
+print(f"Encrypted: {encrypted_text}")
+decrypted_text = steg.aes.decrypt(encrypted_text)
+print(f"Decrypted: {decrypted_text}")
--- App/generate_secrets.py
+++ App/generate_secrets.py
@@ -1,11 +1,21 @@
 import secrets
 import sys
+import random
+import string
+
+DELIMITER_LENGTH = 6
 
 def generate_aes_key():
     return secrets.token_bytes(32) #generate a random key (32 bytes for AES)
 
 def generate_random_iv():
     return secrets.token_bytes(16)  #generate a random initialization vector (16 bytes for AES)
+
+def generate_random_delimiter(length):
+    chars = []
+    for i in range(length):
+        chars.append(random.choice(string.ascii_letters + string.digits + string.punctuation))
+    return ''.join(chars)
 
 
 if len(sys.argv) == 1:
@@ -14,4 +24,5 @@ else:
     PATH = str(sys.argv[1])
 with open(PATH, "w+") as file:
     file.write(f"{str(generate_aes_key())}\n")
-    file.write(str(generate_random_iv()))
+    file.write(f"{str(generate_random_iv())}\n")
+    file.write(generate_random_delimiter(DELIMITER_LENGTH))
